508238dbf1eda83d8cc7c46d461c07f9edb0cf21
Guy Ness <guyness03@gmail.com>
2025-10-30 11:26:14 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,2 +1,13 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    if num1 > num2 and num1 > num3 :
+    return num1
+  elif num2 > num1 and num2 > num3:
+    return num2
+  else:
+    return num3
+
+num1 = 10
+num2 = 5
+num3 = 7
+max_num = find_max_number(num1, num2, num3)
+print("The maximum number is:", max_num)
